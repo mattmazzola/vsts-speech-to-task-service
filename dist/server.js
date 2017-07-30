3cc0351ecb4706b2e0e4106099c8245f1c077775
Matt Mazzola <mattmazzola@live.com>
2017-07-29 19:31:25 -0700

    Update dist files

--- dist/server.js
+++ dist/server.js
@@ -4,6 +4,16 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var node_fetch_1 = require("node-fetch");
 var cors = require("cors");
+var handleResponse = function (response) {
+    return response.json()
+        .then(function (json) {
+        if (!response.ok) {
+            throw new Error(json.message || json.ErrorDescription || JSON.stringify(json));
+        }
+        return json;
+    });
+};
+var handleError = function (res) { return function (e) { return res.status(500).send({ message: e.message }); }; };
 var app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -20,35 +30,37 @@ app.post('/token', function (req, res) {
     var clientSecret = process.env.vstsClientSecret;
     if (typeof clientSecret !== 'string') {
         res.status(500).send({ message: 'Server does not have client secret needed to acquire token' });
+        return;
     }
     var assertion = req.body.authorizationCode ? req.body.authorizationCode : req.body.refreshToken;
     var grantType = req.body.authorizationCode ? 'urn:ietf:params:oauth:grant-type:jwt-bearer' : 'refresh_token';
-    return node_fetch_1.default('https://app.vssps.visualstudio.com/oauth2/token', {
+    node_fetch_1.default('https://app.vssps.visualstudio.com/oauth2/token', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         },
         body: "client_assertion_type=urn:ietf:params:oauth:client-assertion-type:jwt-bearer&client_assertion=" + clientSecret + "&grant_type=" + grantType + "&assertion=" + assertion + "&redirect_uri=" + redirectUri
     })
-        .then(function (response) {
-        return response.json()
-            .then(function (json) {
-            if (!response.ok) {
-                throw new Error(json.ErrorDescription || json.message || JSON.stringify(json));
-            }
-            return json;
-        });
-    })
+        .then(function (r) { return handleResponse(r); })
         .then(function (json) {
         res.send(json);
     })
-        .catch(function (error) {
-        res
-            .status(500)
-            .send({
-            message: error.message
-        });
-    });
+        .catch(handleError(res));
+});
+app.post('/cognitiveservicestoken', function (req, res) {
+    var subscriptionKey = process.env.bingSpeechApiPrimaryKey;
+    if (typeof subscriptionKey !== 'string') {
+        res.status(500).send({ message: 'Server does not have Bing Speech API subscription key needed to acquire token. Contact administrator.' });
+        return;
+    }
+    node_fetch_1.default("https://api.cognitive.microsoft.com/sts/v1.0/issueToken?Subscription-key=" + subscriptionKey, {
+        method: 'POST'
+    })
+        .then(function (r) { return r.text(); })
+        .then(function (token) {
+        res.status(200).send({ token: token });
+    })
+        .catch(handleError(res));
 });
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
